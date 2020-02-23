51add05bbff6570bc20fa0646992487a256a0349
sh201020 <a1426465047@foxmail.com>
2020-02-23 21:39:08 +0800

    本地修改同时更新远程仓库

--- src/main/java/com/accp/springmvc01/action/UserAction.java
+++ src/main/java/com/accp/springmvc01/action/UserAction.java
@@ -14,6 +14,7 @@ public class UserAction {
 		System.out.println("第一次修改");
 		System.out.println("第二次修改");
 		System.out.println("第三次修改");
+		System.out.println("第四次修改");
 		model.addAttribute("User",user);
 		return "/view/index.jsp";
 	}
